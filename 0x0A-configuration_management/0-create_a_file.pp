d026d074fe7701e6acbcc8bc761a27bd443ed2e6
SirJupiter <jupitertoby@gmail.com>
2024-04-19 14:55:24 +0100

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file in /tmp
 
-file {'school':
+file {'/tmp/school':
   content => 'I love puppet',
   group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/school'
+  ensure  => 'file'
 }
