8da82d6f0c42ca251770cdf8c8670caa887215e6
SirJupiter <jupitertoby@gmail.com>
2024-06-15 19:39:00 +0100

    1. User limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,6 @@
-# we are using ApacheBench which is a quite popular tool in the industry. It allows you to simulate HTTP requests to a web server. In this case, I will make 2000 requests to my server with 100 requests at a time. We can see that 943 requests failed; this file should fix our stack so that we get to 0
+# fix nginx to accept and serve more requests
 
-exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_connections [0-9]*/worker_connections 1024/" /etc/nginx/nginx.conf && /bin/sed -i "s/worker_processes [0-9]*/worker_processes auto/" /etc/nginx/nginx.conf && nginx -s reload',
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => 'grep -q "worker_connections" /etc/nginx/nginx.conf',
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-# Ensure the Nginx service is running
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Exec['fix--for-nginx'],
-}
-
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
-# Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
 
-exec { 'create_holberton_user':
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  command => '/bin/chown -R holberton:holberton /etc/nginx'
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
